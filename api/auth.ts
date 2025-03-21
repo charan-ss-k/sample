80bfbf2b464ee88fb15dacb1301b0afe61c9a31a
charan-ss-k <kcharansivasai@gmail.com>
2025-03-21 19:20:02 +0530

    Update auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -15,7 +15,7 @@ const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
   throw new Error("MONGO_URI is not defined in environment variables");
 }
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => {
     console.error("MongoDB connection error:", error);
